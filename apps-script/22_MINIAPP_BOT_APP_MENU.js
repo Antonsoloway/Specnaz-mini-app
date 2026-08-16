968a08d7d57ccf35f1d98270662f342053bb0a46
Antonsoloway <soloway3852@gmail.com>
2026-08-16 17:46:39 +0300

    v0.5.36 refresh Telegram menu cache bust

--- apps-script/22_MINIAPP_BOT_APP_MENU.js
+++ apps-script/22_MINIAPP_BOT_APP_MENU.js
@@ -6,7 +6,7 @@ function MINIAPP_setupBotAppMenu() {
   var token=String(props.getProperty('TELEGRAM_BOT_TOKEN')||props.getProperty('BOT_TOKEN')||'').trim();
   if(!token) throw new Error('Telegram bot token property is missing');
   var gasUrl=String(ScriptApp.getService().getUrl()||'').trim();
-  var appUrl=MINIAPP_BOT_APP_URL+'?cb=20260816-1736';
+  var appUrl=MINIAPP_BOT_APP_URL+'?cb=20260816-1747';
   if(gasUrl) appUrl+='&gas='+encodeURIComponent(gasUrl);
   var api='https://api.telegram.org/bot'+token+'/';
   var commands=MINIAPP_botAppMenuCall_(api+'setMyCommands',{commands:[{command:'start',description:'🚀 Открыть приложение'}],scope:{type:'all_private_chats'}});
