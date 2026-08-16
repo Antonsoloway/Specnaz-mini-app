00c3e4dea1e3788130d0aea8581767d319e07634
Antonsoloway <soloway3852@gmail.com>
2026-08-16 15:21:09 +0300

    Point bot menu to Mini App v0.5.31

--- apps-script/22_MINIAPP_BOT_APP_MENU.js
+++ apps-script/22_MINIAPP_BOT_APP_MENU.js
@@ -1,13 +1,13 @@
 /*
  * Royal CRM / Таблица ЧП
  * 22_MINIAPP_BOT_APP_MENU.js
- * v1.0.28
+ * v1.0.29
  *
  * Configures the bot's private-chat menu button to launch the Mini App directly.
  */
 
-var MINIAPP_BOT_APP_MENU_VERSION = '1.0.28';
-var MINIAPP_BOT_APP_URL = 'https://antonsoloway.github.io/Specnaz-mini-app/app-v0530-r2.html';
+var MINIAPP_BOT_APP_MENU_VERSION = '1.0.29';
+var MINIAPP_BOT_APP_URL = 'https://antonsoloway.github.io/Specnaz-mini-app/app-v0531.html';
 
 function MINIAPP_setupBotAppMenu() {
   var props = PropertiesService.getScriptProperties();
@@ -15,7 +15,7 @@ function MINIAPP_setupBotAppMenu() {
   if (!token) throw new Error('Telegram bot token property is missing');
 
   var gasUrl = String(ScriptApp.getService().getUrl() || '').trim();
-  var appUrl = MINIAPP_BOT_APP_URL + '?cb=20260816-1505';
+  var appUrl = MINIAPP_BOT_APP_URL + '?cb=20260816-1516';
   if (gasUrl) appUrl += '&gas=' + encodeURIComponent(gasUrl);
 
   var api = 'https://api.telegram.org/bot' + token + '/';
