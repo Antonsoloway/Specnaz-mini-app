a127a892fd341d65bd457e202d6c67944c3c8698
Antonsoloway <soloway3852@gmail.com>
2026-08-23 12:43:01 +0300

    Add v0.6.1 changelog module

---
+++ changelog-v0601.js
@@ -0,0 +1,9 @@
+/* Royal CRM Mini App — changelog v0.6.1 */
+(() => {
+  window.RoyalChangelog0601 = {
+    version: '0.6.1',
+    items: [
+      'Переход из профиля участника в карточку своей команды.'
+    ]
+  };
+})();
